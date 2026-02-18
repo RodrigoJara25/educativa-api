79c9cc5892a73d346739369b4561248e28c5d299
RodrigoJara25 <r.jara.e7@gmail.com>
2026-02-18 15:51:20 -0500

    feature: subcategory.dto.js

---
+++ src/dao/dto/subcategory.dto.js
@@ -0,0 +1,10 @@
+export default class SubcategoryDTO {
+    constructor(subcategory) {
+        this._id = subcategory._id;
+        this.nombre = subcategory.nombre;
+        this.categoria = subcategory.categoria;  // Vendrá populado con { _id, nombre, tipo }
+        this.orden = subcategory.orden;
+        this.createdAt = subcategory.createdAt;
+        this.updatedAt = subcategory.updatedAt;
+    }
+}
